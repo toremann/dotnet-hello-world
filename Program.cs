040737f0bf9cea89088bc3b61053e10fcfc8319f
toremann <50668920+toremann@users.noreply.github.com>
2023-03-20 11:59:25 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is ur name?");
+            string str = Console.ReadLine();
+            Console.WriteLine("Hello " + str);
+        }
+    }
+}
+
